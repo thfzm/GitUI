90823eb8ee08de450bc35e1bd1e17c605a1ab67c
Park SunYou <108808013+thfzm@users.noreply.github.com>
2026-04-27 22:26:21 +0900

    Initial upload via GitUI

---
+++ GitUI/Views/RepoDetailView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace GitUI.Views;
+
+public partial class RepoDetailView : UserControl
+{
+    public RepoDetailView()
+    {
+        InitializeComponent();
+    }
+}
