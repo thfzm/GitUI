aba235256df5bf82624dd58af7839a1a3bccaef4
Park SunYou <108808013+thfzm@users.noreply.github.com>
2026-04-28 17:12:27 +0900

    Update via GitUI

--- GitUI/Views/Tabs/WatchTab.xaml.cs
+++ GitUI/Views/Tabs/WatchTab.xaml.cs
@@ -1,8 +1,20 @@
+using System.Windows;
 using System.Windows.Controls;
+using GitUI.ViewModels.Tabs;
 
 namespace GitUI.Views.Tabs;
 
 public partial class WatchTab : UserControl
 {
-    public WatchTab() { InitializeComponent(); }
+    public WatchTab()
+    {
+        InitializeComponent();
+        Loaded += WatchTab_Loaded;
+    }
+
+    private void WatchTab_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is WatchTabViewModel vm)
+            vm.VerifyFolderHealth();
+    }
 }
